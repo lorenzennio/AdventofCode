46329ecb0f0c796663fc691da9f2bfbf6e8d0eae
Lorenz Gaertner <lorenz.gaertner@physik.uni-muenchen.de>
2023-12-09 12:04:03 +0100

    automatic solution function generation

--- 2023/src/aoc23.jl
+++ 2023/src/aoc23.jl
@@ -6,15 +6,16 @@ module aoc23
     readInput(day::Int) = aoc.readInput(day, @__DIR__)
     export readInput
 
-    solvedDays = [1, 2, 3]
 
-    # Include the source files:
+    solvedDays = [parse(Int, match(r"\d+", file).match) for file in Base.Filesystem.readdir(@__DIR__) if isnothing(match(r"aoc", file))]
+    
+		# Include the source files:
     for day in solvedDays
         ds = @sprintf("%02d", day)
         include(joinpath(@__DIR__, "day$ds.jl"))
     end
 
-    # Export a function `dayXY` for each day:
+    # Export a function `solutionXX` for each day:
     for d in solvedDays
         modSymbol = Symbol(@sprintf("day%02d", d))
         solSymbol = Symbol(@sprintf("solution%02d", d))
